8b5ac4972cfc962574687a1d81997a5e7dcbda1f
kaur123 <gunnypari@gmail.com>
2018-08-07 15:29:31 -0700

    first commit

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static( __dirname + '/public/dist/public' ));
 const router = require("./server/routes");
 // router(app)
 
-app.all("*", (req,res,next) => {
-    res.sendFile(path.resolve("./public/dist/public/index.html"))
+app.all("/*", (req,res,next) => {
+    res.sendFile(path.resolve(__dirname + "/public/dist/public/index.html"))
 });
 
   
